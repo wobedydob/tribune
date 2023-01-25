b3026578b64a3fca9d30cb31a4601415a4b8151d
Wob Jelsma <w.jelsma@getinteractive.nl>
2023-01-25 03:18:52 +0100

    implemented basic NewspaperItem and NewsColumn data managing

--- assets/js/components/Newspaper/NewsColumn.vue
+++ assets/js/components/Newspaper/NewsColumn.vue
@@ -2,33 +2,9 @@
 
     <div class="column">
         <div class="head">
-            <span class="headline hl1">The Witch Queen of New Orleans</span>
-            <p><span class="headline hl4">by CHARLES OAKLEY</span></p>
+            <span class="headline hl1">{{ this.headline }}</span>
+            <p><span class="headline hl4">by {{ this.author }}</span></p>
         </div>
-
-        <p>
-
-            I'm gonna tell you a story
-            Strange as it might seem
-            Of zombie voodoo gris-gris
-            And the Witch Queen of New Orleans
-            She lived in a world of magic
-            Possessed by the devil's skew
-            From her shack near the swampland
-            Made of mud-pie brick
-            Marie stirred her witch's brew
-            Dime or a nickel, anyone could buy
-            Voodoo of any kind
-            She had potions and lotions
-            Herbs and tanna leaves
-            Guaranteed to blow your mind
-            Early one morning, into murky swamp dew
-            Vanished Marie with hate in her eyes
-            Though she'll never return
-            All the Cajuns knew
-            A witch queen never dies
-
-        </p>
 
     </div>
 
@@ -36,6 +12,25 @@
 
 <script>
 export default {
-    name: "NewsColumn"
+    name: "NewsColumn",
+
+    props: ['article'],
+
+    data() {
+        return {
+            headline: '',
+            body: [],
+            author: ''
+        }
+    },
+
+    created() {
+        if (this.article) {
+            this.headline = this.article.headline;
+            this.body = this.article.body;
+            this.author = this.article.author
+        }
+    },
+
 }
 </script>
--- assets/js/components/Newspaper/NewspaperItem.vue
+++ assets/js/components/Newspaper/NewspaperItem.vue
@@ -8,21 +8,7 @@
 
             <div class="columns">
 
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
-
-            </div>
-
-            <div class="columns">
-
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
-                <news-column></news-column>
+                <news-column v-if="newspaper"  v-for="article in this.articles" :key="article.id" :article="article"></news-column>
 
             </div>
 
@@ -35,8 +21,47 @@
 
 import NewspaperHeadline from "./NewspaperHeadline.vue";
 import NewsColumn from "./NewsColumn.vue";
+
 export default {
     name: "NewspaperItem",
+
+    props: ['newspaper'],
+
+    data() {
+        return {
+            articles: {}
+        }
+    },
+
+    watch: {
+        newspaper: {
+            handler: function(newValue, oldValue) {
+                if (newValue) {
+                    this.articles = newValue.articles;
+                }
+            },
+            deep: true
+        }
+    },
+
+    methods: {
+
+        getNewspaper(id = 0) {
+
+            return this.getById(this.newspapers, id)
+
+        },
+
+        getNewest(data) {
+            return data.slice(-1)[0];
+        },
+
+        getById(data, id) {
+            return data[id];
+        }
+
+    },
+
     components: {
         NewspaperHeadline,
         NewsColumn
